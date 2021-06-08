a2814588d6353b6cc621d334b15fd6bb93cf94c0
Morilli <35152647+Morilli@users.noreply.github.com>
2021-06-08 05:07:18 +0200

    > 100 lines removed\n> massive functionality added
    - i know the "\n" doesn't work
    - this works surprisingly
    - probably leaks memory in lots of places still, should be fixed

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 
 CFLAGS ?= -std=gnu18 -Wall -Wextra -O3 -flto
-LDFLAGS ?= -O3 -flto -lcomctl32 -lUxTheme -static -mwindows
+LDFLAGS ?= -O3 -flto -lcomctl32 -lUxTheme -lgdi32 -lComdlg32 -static bnk-extract.dll
 target := gui.exe
 
 all: $(target)
@@ -8,12 +8,12 @@ all: $(target)
 
 object_files := test.o templatewindow.o resource.res
 
-test.o: resource.h
+test.o: resource.h templatewindow.h
 resource.res: resource.rc resource.h icon.ico
 	windres --output-format=coff resource.rc $@
 
 
-$(target): $(object_files)
+$(target): $(object_files) bnk-extract.dll
 	$(CC) $(CFLAGS) $^ $(LDFLAGS) -o $@
 
 
--- test.c
+++ test.c
@@ -8,68 +8,46 @@
 
 #include "resource.h"
 #include "templatewindow.h"
+#include "list.h"
 
-const char* string_array[] = {"string1", "secondstring", "anothertext", "uwu", "onii-chan", "bruder muss los"};
+typedef struct stringWithChildren {
+    char* string;
+    LIST(struct stringWithChildren) children;
+} StringWithChildren;
+
+__declspec(dllimport) StringWithChildren* bnk_extract(int argc, char* argv[]);
+
+#define function void*
+
+void InsertStringToTreeview(HWND Treeview, StringWithChildren* element, HTREEITEM parent)
+{
+    TVINSERTSTRUCT newItemInfo = {
+        .hInsertAfter = TVI_LAST,
+        .hParent = parent,
+        .item = {
+            .mask = TVIF_TEXT,
+            .pszText = element->string
+        }
+    };
+    HTREEITEM newItem = (HTREEITEM) SendMessage(Treeview, TVM_INSERTITEM, 0, (LPARAM) &newItemInfo);
+
+    for (uint32_t i = 0; i < element->children.length; i++) {
+        InsertStringToTreeview(Treeview, &element->children.objects[i], newItem);
+    }
+}
+
 const char g_szClassName[] = "myWindowClass";
 static HINSTANCE me;
-
-#define WM_RETICULATE_SPLINES (WM_USER + 0x0001)
-
-static HWND edit_text;
 static HWND mainWindow;
-
-void* updateProgressBar(void* _args)
-{
-    HWND progress_bar;
-    memcpy(&progress_bar, _args, sizeof(HWND));
-
-    SendMessage(progress_bar, PBM_SETRANGE, 0, MAKELONG(0, 5000));
-    for (int i = 0; i < 500; i++) {
-        // SendMessage(progress_bar, PBM_STEPIT, 0, 0);
-        // SendMessage(progress_bar, PBM_STEPIT, 0, 0);
-        // SendMessage(progress_bar, PBM_STEPIT, 0, 0);
-        // SendMessage(progress_bar, PBM_STEPIT, 0, 0);
-        SendMessage(progress_bar, PBM_STEPIT, 0, 0);
-        Sleep(rand() % 100);
-    }
-
-    while (true) {
-        HWND new_progress_bar = (HWND) SendMessage(mainWindow, WM_RETICULATE_SPLINES, 0, 0);
-        // HWND new_progress_bar = CreateWindowEx(0, PROGRESS_CLASS, NULL, WS_CHILD | WS_VISIBLE | PBS_SMOOTH, rand() % 500, rand() % 500, 200, GetSystemMetrics(SM_CYVSCROLL), mainWindow, NULL, me, NULL);
-        SendMessage(new_progress_bar, PBM_SETRANGE, 0, MAKELONG(0, 1000));
-        for (int i = 0; i < 100; i++) {
-            SendMessage(new_progress_bar, PBM_STEPIT, 0, 0);
-            Sleep(rand() % 100);
-        }
-    }
-
-    HWND second_progress_bar;
-    memcpy(&second_progress_bar, _args + sizeof(HWND), sizeof(HWND));
-    SendMessage(second_progress_bar, PBM_SETRANGE, 0, MAKELONG(0, 5000));
-    for (int i = 0; i < 500; i++) {
-        // SendMessage(progress_bar, PBM_STEPIT, 0, 0);
-        // SendMessage(progress_bar, PBM_STEPIT, 0, 0);
-        // SendMessage(progress_bar, PBM_STEPIT, 0, 0);
-        // SendMessage(progress_bar, PBM_STEPIT, 0, 0);
-        SendMessage(second_progress_bar, PBM_STEPIT, 0, 0);
-        Sleep(rand() % 100);
-    }
-
-
-    return NULL;
-}
-
-int get_number()
-{
-    static int number = 1;
-    number *= 2;
-    // number++;
-    return number;
-}
-
-int windows_created;
+static HWND BinTextBox, AudioTextBox, EventsTextBox;
+static HWND BinFileSelectButton, AudioFileSelectButton, EventsFileSelectButton, GoButton;
+static HWND treeview;
 
 pthread_t worker_thread;
+HBRUSH text_background_color;
+
+#define turquoise (RGB(64, 224, 208))
+#define dark_turquoise (RGB(0, 206, 209))
 
 
 // Step 4: the Window Procedure
@@ -77,88 +55,50 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch(msg)
     {
-        case WM_RETICULATE_SPLINES: {
-            HWND newProgressBar = CreateWindowEx(0, PROGRESS_CLASS, NULL, WS_CHILD | WS_VISIBLE | PBS_SMOOTH, rand() % 750, rand() % 750, 200, GetSystemMetrics(SM_CYVSCROLL), mainWindow, NULL, me, NULL);
-            return (LRESULT) newProgressBar;
-        }
+        case WM_CTLCOLORSTATIC:
+            if (!text_background_color) text_background_color = CreateSolidBrush(turquoise);
+            SetBkColor((HDC) wParam, dark_turquoise);
+            return (LRESULT) text_background_color;
         case WM_CLOSE: {
             MessageBox(hwnd, "Fenster wird geschlossen...", NULL, MB_ICONQUESTION);
-            // INITCOMMONCONTROLSEX icex = {
-            //     .dwICC = ICC_PROGRESS_CLASS,
-            //     .dwSize = sizeof(INITCOMMONCONTROLSEX)
-            // };
-            // InitCommonControlsEx(&icex);
-            // HWND progress_bar = CreateWindowEx(0, PROGRESS_CLASS, NULL, WS_CHILD | WS_VISIBLE, 10, 10, 100, GetSystemMetrics(SM_CYVSCROLL), hwnd, NULL, me, NULL);
-
-            // for (int i = 0; i < 10; i++) {
-            //     SendMessage(progress_bar, PBM_STEPIT, 0, 0);
-            //     Sleep(1000);
-            // }
             DestroyWindow(hwnd);
-            // char integer[18];
-            // sprintf(integer, "%d", get_number());
-            // sprintf(integer + 10, "%d", rand());
-            // WinMain(NULL, NULL, integer, SW_SHOWNORMAL);
             break;
         }
         case WM_DESTROY:
-            // MessageBox(hwnd, "nice one", "what", 0);
             PostQuitMessage(0);
             break;
-        // case
         case WM_COMMAND:
-            // printf("got here wm_command\n");
             if (lParam && HIWORD(wParam) == BN_CLICKED) {
-                char* IQ_text = calloc(100, 1);
-                int iq;
-                int unlikely = 0;
-                INITCOMMONCONTROLSEX icex = {
-                    .dwICC = ICC_PROGRESS_CLASS,
-                    .dwSize = sizeof(INITCOMMONCONTROLSEX)
-                };
-                InitCommonControlsEx(&icex);
-                HWND progress_bar_text = CreateWindowEx(0, "STATIC", NULL, WS_CHILD | WS_VISIBLE, 10, 0, 2000, GetSystemMetrics(SM_CYVSCROLL), hwnd, NULL, me, NULL);
-                SetWindowText(progress_bar_text, "Deleting system32...");
-                HWND progress_bar = CreateWindowEx(0, PROGRESS_CLASS, NULL, WS_CHILD | WS_VISIBLE | PBS_SMOOTH, 10, 20, 500, GetSystemMetrics(SM_CYVSCROLL), hwnd, NULL, me, NULL);
-                void* to_leak = malloc(sizeof(HWND) * 2);
-                memcpy(to_leak, &progress_bar, sizeof(HWND));
-                HWND second_progress_bar = CreateWindowEx(WS_EX_STATICEDGE, PROGRESS_CLASS, NULL, WS_CHILD | WS_VISIBLE | PBS_SMOOTH | WS_CLIPSIBLINGS, 500+10, 20, 500, GetSystemMetrics(SM_CYVSCROLL), hwnd, NULL, me, NULL);
-                SetWindowTheme(second_progress_bar, NULL, NULL);
-                LONG style = GetWindowLong(second_progress_bar, GWL_EXSTYLE);
-                style &= ~WS_EX_STATICEDGE;
-                SetWindowLong(second_progress_bar, GWL_EXSTYLE, style);
-                SendMessage(second_progress_bar, PBM_SETBKCOLOR, 0, RGB(255, 255, 255));
-                memcpy(to_leak + sizeof(HWND), &second_progress_bar, sizeof(HWND));
-                // mainWindow = hwnd;
-                pthread_create(&worker_thread, NULL, updateProgressBar, to_leak);
-                // pthread_detach(worker_thread);
-
-                while (1) {
-                    iq = rand() % 150;
-                    if (iq < 70) continue;
-                    int distance_from_100 = abs(iq - 100);
-                    unlikely += distance_from_100;
-                    if (distance_from_100 <= unlikely / 4) break;
+                if ((HWND) lParam == GoButton) {
+                    char binPath[256], audioPath[256], eventsPath[256];
+                    GetWindowText(BinTextBox, binPath, 255);
+                    GetWindowText(AudioTextBox, audioPath, 255);
+                    GetWindowText(EventsTextBox, eventsPath, 255);
+                    char* bnk_extract_args[] = {"", "-b", binPath, "-a", audioPath, "-e", eventsPath, "-vv", NULL};
+                    StringWithChildren* structuredWems = bnk_extract(ARRAYSIZE(bnk_extract_args)-1, bnk_extract_args);
+                    if (structuredWems)
+                        InsertStringToTreeview(treeview, structuredWems, TVI_ROOT);
+                    else {
+                        MessageBox(mainWindow, "An error occured while parsing the provided audio files. Most likely you provided none or not the correct files.\nIf there was a log window you could actually read the error message LOL", "Failed to read audio files", MB_ICONERROR);
+                    }
+                    // proceed to leak all allocated memory here
+                    return 0;
                 }
-                // FILE* textfileiguess = fopen("G:\\Dokumente\\Google Takeout.7z", "rb");
-                // fseek(textfileiguess, 0, SEEK_END);
-                // size_t file_size = ftello64(textfileiguess);
-                // char* text = malloc(file_size + 1);
-                // rewind(textfileiguess);
-                // fread(text, 1, file_size, textfileiguess);
-                // fclose(textfileiguess);
-                // sprintf(IQ_text, "\r\nCalculated IQ: %d", iq);
-                SetWindowText(edit_text, IQ_text);
+                char fileNameBuffer[256] = {0};
+                OPENFILENAME fileNameInfo = {
+                    .lStructSize = sizeof(OPENFILENAME),
+                    .hwndOwner = mainWindow,
+                    .lpstrFile = fileNameBuffer,
+                    .nMaxFile = 256
+                };
+                if (GetOpenFileName(&fileNameInfo)) {
+                    if ((HWND) lParam == BinFileSelectButton) SetWindowText(BinTextBox, fileNameBuffer);
+                    if ((HWND) lParam == AudioFileSelectButton) SetWindowText(AudioTextBox, fileNameBuffer);
+                    if ((HWND) lParam == EventsFileSelectButton) SetWindowText(EventsTextBox, fileNameBuffer);
+                }
             }
             break;
-        case WM_NCHITTEST: {
-            UINT nPos = DefWindowProc(hwnd, msg, wParam, lParam);
-            if ( nPos == HTCAPTION ) return HTNOWHERE;
-            return nPos;
-            break;
-        }
         default:
-            // printf("got here with msg %d\n", msg);
             return DefWindowProc(hwnd, msg, wParam, lParam);
     }
     return 0;
@@ -167,6 +107,10 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 int WINAPI WinMain(HINSTANCE hInstance, __attribute__((unused)) HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     srand(time(NULL));
+    // init used common controls
+    InitCommonControlsEx(&(INITCOMMONCONTROLSEX) {.dwSize = sizeof(INITCOMMONCONTROLSEX), .dwICC = ICC_PROGRESS_CLASS});
+    InitCommonControlsEx(&(INITCOMMONCONTROLSEX) {.dwSize = sizeof(INITCOMMONCONTROLSEX), .dwICC = ICC_TREEVIEW_CLASSES});
+
     me = hInstance;
     WNDCLASSEX wc;
     HWND hwnd;
@@ -174,8 +118,8 @@ int WINAPI WinMain(HINSTANCE hInstance, __attribute__((unused)) HINSTANCE hPrevI
 
     //Step 1: Registering the Window Class
 
-    int icon_size = GetSystemMetrics(SM_CXICON);
-    int iconsm_size = GetSystemMetrics(SM_CXSMICON);
+    int icon_size    = GetSystemMetrics(SM_CXICON);
+    int iconsm_size  = GetSystemMetrics(SM_CXSMICON);
     wc.cbSize        = sizeof(WNDCLASSEX);
     wc.style         = 0;
     wc.lpfnWndProc   = WndProc;
@@ -185,7 +129,7 @@ int WINAPI WinMain(HINSTANCE hInstance, __attribute__((unused)) HINSTANCE hPrevI
     wc.hIcon         = LoadImage(hInstance, MAKEINTRESOURCE(IDI_MYICON), IMAGE_ICON, icon_size, icon_size, 0);
     wc.hIconSm       = LoadImage(hInstance, MAKEINTRESOURCE(IDI_MYICON), IMAGE_ICON, iconsm_size, iconsm_size, 0);
     wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOWFRAME);
     wc.lpszMenuName  = MAKEINTRESOURCE(IDR_MYMENU);
     wc.lpszClassName = hInstance ? g_szClassName : &lpCmdLine[10];
 
@@ -196,97 +140,61 @@ int WINAPI WinMain(HINSTANCE hInstance, __attribute__((unused)) HINSTANCE hPrevI
     }
 
     // Step 2: Creating the Window
-    for (; windows_created < (hInstance ? 1 : strtol(lpCmdLine, NULL, 10)); windows_created++) {
-        // int x = ((windows_created / 3) * 480 * 9 / 7) % 1920;
-        // int y = (360 * 9 / 7 * windows_created) % 1080;
-        int w = 300;
-        int h = 200;
-        // int x = (425 * windows_created) % 1920;
-        // int y = (317 * windows_created) % 1080;
-        int x = CW_USEDEFAULT;
-        int y = CW_USEDEFAULT;
-        RECT rect;
-        rect.left   = x;
-        rect.top    = y;
-        rect.right  = x + w;
-        rect.bottom = y + h;
-        UINT style = WS_OVERLAPPEDWINDOW ;
-        AdjustWindowRectEx(&rect, style, 0, 0);
-        // DwmGetWindowAttribute()
-        char window_name[30] = "high quality gui ";
-        sprintf(window_name + 21, "%d", windows_created);
-        hwnd = CreateWindowEx(
-            WS_EX_CLIENTEDGE | WS_EX_LAYERED,
-            g_szClassName,
-            window_name,
-            style,
-            CW_USEDEFAULT, CW_USEDEFAULT, rect.right-rect.left, rect.bottom-rect.top,
-            NULL, NULL, hInstance, NULL);
-        mainWindow = hwnd;
-        // AdjustWindowRect(&rect, 0, 0);
-        // SetWindowPos(hwnd, NULL, rect.left, rect.top, rect.right-rect.left, rect.bottom-rect.top, 0);
-        SetLayeredWindowAttributes(hwnd, 0, 230, LWA_ALPHA);
-
-        // button
-        for (int i = 0; i < 100; i++) {
-
-            HWND childWindow = CreateEmptyWindow(hwnd, hInstance, i, i, 100, 100, "testwindownew", WndProc);
-
-
-        CreateWindow(
-            "BUTTON",  // Predefined class; Unicode assumed
-            "Ok",      // Button text
-            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_CHECKBOX,  // Styles
-            rand() % 1000,         // x position
-            rand() % 1000,         // y position
-            100,        // Button width
-            60,        // Button height
-            childWindow,     // Parent window
-            (HMENU) IDOK,       // No menu.
-            hInstance,
-            NULL
-        );      // Pointer not needed.
-        CreateWindow(
-            "BUTTON",  // Predefined class; Unicode assumed
-            "Ok",      // Button text
-            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON | WS_CLIPSIBLINGS,  // Styles
-            rand() % 1000,         // x position
-            rand() % 1000,         // y position
-            100,        // Button width
-            60,        // Button height
-            hwnd,     // Parent window
-            NULL,       // No menu.
-            hInstance,
-            NULL
-        );      // Pointer not needed.
-        SetClassLongPtr(childWindow, GWLP_WNDPROC, (LONG_PTR) WndProc);
-
-        // window.
-        }
-
-        // text above the button
-        edit_text = CreateWindow(
-            "EDIT",
-            NULL,
-            WS_BORDER | WS_CHILD | WS_VISIBLE | ES_CENTER | ES_READONLY | ES_MULTILINE,
-            80,30,140,55,
-            hwnd,
-            NULL,
-            hInstance,
-            NULL
-        );
-        printf("x pos: %d, y pos: %d\n", (windows_created / 3) * 480, (360 * windows_created) % 1080);
-
-        if(hwnd == NULL)
-        {
-            MessageBox(NULL, "Window Creation Failed!", "Error!",
-                MB_ICONEXCLAMATION | MB_OK);
-            return 0;
-        }
-
-        ShowWindow(hwnd, nCmdShow);
-        UpdateWindow(hwnd);
+    int width = 800;
+    int height = 500;
+    int x = CW_USEDEFAULT;
+    int y = CW_USEDEFAULT;
+    RECT rect;
+    rect.left   = x;
+    rect.top    = y;
+    rect.right  = x + width;
+    rect.bottom = y + height;
+    UINT style = WS_OVERLAPPEDWINDOW ;
+    AdjustWindowRectEx(&rect, style, 0, 0);
+    char window_name[] = "high quality gui uwu";
+    hwnd = CreateWindowEx(
+        WS_EX_CLIENTEDGE | WS_EX_LAYERED,
+        g_szClassName,
+        window_name,
+        style,
+        CW_USEDEFAULT, CW_USEDEFAULT, rect.right-rect.left, rect.bottom-rect.top,
+        NULL, NULL, hInstance, NULL);
+    if(hwnd == NULL)
+    {
+        MessageBox(NULL, "Window Creation Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
     }
+    mainWindow = hwnd;
+    SetLayeredWindowAttributes(hwnd, 0, 230, LWA_ALPHA);
+
+    // create a tree view
+    treeview = CreateWindowEx(0, WC_TREEVIEW, NULL, WS_VISIBLE | WS_CHILD | TVS_HASLINES | TVS_HASBUTTONS | TVS_LINESATROOT, 0, 80, 450, 400, hwnd, NULL, hInstance, NULL);
+
+    // three text fields for the bin, audio bnk/wpk and events bnk
+    BinTextBox = CreateWindowEx(0, "EDIT", NULL, WS_CHILD | WS_VISIBLE | ES_LEFT | ES_AUTOHSCROLL | WS_BORDER, 115, 10, 450, 20, hwnd, NULL, hInstance, NULL);
+    AudioTextBox = CreateWindowEx(0, "EDIT", NULL, WS_CHILD | WS_VISIBLE | ES_LEFT | ES_AUTOHSCROLL | WS_BORDER, 115, 30, 450, 20, hwnd, NULL, hInstance, NULL);
+    EventsTextBox = CreateWindowEx(0, "EDIT", NULL, WS_CHILD | WS_VISIBLE | ES_LEFT | ES_AUTOHSCROLL | WS_BORDER, 115, 50, 450, 20, hwnd, NULL, hInstance, NULL);
+
+    // buttons yay
+    BinFileSelectButton = CreateWindowEx(0, "BUTTON", "select bin file", WS_TABSTOP | WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 0, 10, 110, 20, hwnd, NULL, hInstance, NULL);
+    AudioFileSelectButton = CreateWindowEx(0, "BUTTON", "select audio file", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 0, 30, 110, 20, hwnd, NULL, hInstance, NULL);
+    EventsFileSelectButton = CreateWindowEx(0, "BUTTON", "select events file", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 0, 50, 110, 20, hwnd, NULL, hInstance, NULL);
+    GoButton = CreateWindowEx(0, "BUTTON", "Parse files", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 600, 28, 130, 24, hwnd, NULL, hInstance, NULL);
+    // disable the ugly selection outline of the text when the button gets pushed
+    SendMessage(BinFileSelectButton, WM_CHANGEUISTATE, MAKELONG(UIS_SET, UISF_HIDEFOCUS), 0);
+    SendMessage(AudioFileSelectButton, WM_CHANGEUISTATE, MAKELONG(UIS_SET, UISF_HIDEFOCUS), 0);
+    SendMessage(EventsFileSelectButton, WM_CHANGEUISTATE, MAKELONG(UIS_SET, UISF_HIDEFOCUS), 0);
+    SendMessage(GoButton, WM_CHANGEUISTATE, MAKELONG(UIS_SET, UISF_HIDEFOCUS), 0);
+    // snatch the nice font from the treeview and slap it onto the textboxes
+    // note that there is likely a better way to do this but this works i guess so
+    LRESULT fontHandle = SendMessage(treeview, WM_GETFONT, 0, 0);
+    SendMessage(BinTextBox, WM_SETFONT, fontHandle, 0);
+    SendMessage(AudioTextBox, WM_SETFONT, fontHandle, 0);
+    SendMessage(EventsTextBox, WM_SETFONT, fontHandle, 0);
+
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
 
     // Step 3: The Message Loop
     while(GetMessage(&Msg, NULL, 0, 0) > 0)
